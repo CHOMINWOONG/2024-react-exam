48b360949f4f38de2d8eaf24b181e1b9d639107e
CHOMINWOONG <alsdnd0701@naver.com>
2024-02-05 21:27:10 +0900

    다양한 증감

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import React, { useState } from 'react';
+
 function App() {
-  return <div>안녕!!!!!!!!</div>
+  const [num, setNum] = useState(0);
+  const onBtnIncreaseClicked = () => {
+    setNum( num + 10 );
+  }
+  const onBtnIncreaseClicke = () => {
+    setNum( num - 10 );
+  }
+
+
+
+  return (
+    <>
+      NUM : {num}
+      <br/>
+      <button onClick={() => setNum(num + 1)}>+1</button>
+      <button onClick={() => setNum(num - 1)}>-1</button>
+      <button onClick={(onBtnIncreaseClicked)}>+10</button>
+      <button onClick={(onBtnIncreaseClicke)}>-10</button>
+    </>
+  );
 }
 
 export default App;

94705ae9c67e8e7a517e3821884926e1f199075f
CHOMINWOONG <alsdnd0701@naver.com>
2024-03-15 21:45:13 +0900

    반응형 상품리스트 만들기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
-import ProductListItem from './productListltem';
+import ProductList from './ProductList';
 
 export default function App(){
 
 
 
 return <>
-  <div className='flex gap-3'>
-    <ProductListItem imgNo={1} name="MAC" price="4,300,000"/>
-    <ProductListItem imgNo={2} name="MAC AIR" price="2,300,000"/>
-    <ProductListItem imgNo={3} name="MAC PRO" price="3,300,000"/>
-  </div>
+
+  <ProductList className="container mx-auto"/>    
 </>
 }
 

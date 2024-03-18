33c5fadcd91f448855388e0efa7d5e3ef66d0d1c
CHOMINWOONG <alsdnd0701@naver.com>
2024-03-18 21:23:17 +0900

    다양한 정보입력

--- src/App.js
+++ src/App.js
@@ -2,51 +2,60 @@ import React, { useState } from 'react';
 
 export default function App(){
 
+  const [recordedNos, setRecordedNos] = useState([10, 20, 30]);
 
-   const [no, setNo] = useState(0);
+  const saveNo = (form) => {
+    form.no.value = form.no.value.trim();
 
-    const [recordedNo, setRecordedNo] = useState([]);
-    const saveNo = (e) => {
-      e.preventDefault();
-      // 기본적으로 이벤트를 막는다
+    if (form.no.value.length) {
+      alert("숫자를 입력해주세요.");
+      return;
+    }
 
-      if ( no == '') {
-        alert('숫자를 입력해주세요.');
-        return;
-      }
+    setRecordedNos([...recordedNos, form.no.value]);
+    form.no.value = '';
+    form.no.focus();
+  };
 
-    setRecordedNo([...recordedNo, no]);
-    setNo('');
-    
-  }
+  //const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
+  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
 
-  const li = recordedNo.map((el, index) => <li key={index}>{el}</li>)
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo();
+        }}
+      >
+        <input
+          type="number"
+          name="no"
+          value={no}
+          onChange={(e) => setNo(e.target.valueAsNumber)}
+        />
+        <button type="submit">기록</button>
+      </form>
 
-  return <>
-    <form>
-      <input type="number" className="input input board" value={no} onChange={(e) => setNo(e.target.valueAsNumber)}/>
-      <button type="submit" className="btn" onClick={saveNo}>기록</button>
-    </form>
+      <hr />
 
-    <hr />
-    
-    <div className='text-4xl'>기록된 숫자 : v1</div>
-    {recordedNo.join(',')}
-    
-    <div className='text-4xl'>기록된 숫자 : v2</div>
-    <ul>{li}</ul>
+      <h1>기록된 숫자 v1</h1>
+      {recordedNos.join(",")}
 
-    <div className='text-4xl'>기록된 숫자 : v3</div>
-    <ul>
-      {recordedNo.map((el, index) => <li key={index}>{el}</li>)}
-    </ul>
-  </>
+      <hr />
 
-  // 반복문 없이 'map' 사용하여 배열 만들기
-  // index를 map 앞에 붙이면 index값도 불러올 수 있음.
-            // [0,1,2,3,4,5,6,7] *index*
-  // const nums = [1,2,3,4,5,6,7,8];
-  // const arr = nums.map((el, index) => <li key={index}>내용 {el}</li>);
+      <h1>기록된 숫자 v2</h1>
+      <ul>{li}</ul>
 
+      <hr />
+
+      <h1>기록된 숫자 v2-2</h1>
+      <ul>
+        {recordedNos.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 

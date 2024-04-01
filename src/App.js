e2d39b7ff9b109fecdc06a46fc125c04fa306f7b
CHOMINWOONG <alsdnd0701@naver.com>
2024-04-01 19:44:27 +0900

    간단한 버전의 CRUD

--- src/App.js
+++ src/App.js
@@ -2,125 +2,45 @@ import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react'
 
 
 
-function OrderMainFood({setMainFoodCount, mainFoodCount}) {
-  return(
-  <>
-   <h2>메인 (수량 : {mainFoodCount})</h2>
-    <div>
-      <button className="btn btn-primary" onClick={() => setMainFoodCount(mainFoodCount + 1)}>증가</button>
-      <button className="btn btn-primary" onClick={() => setMainFoodCount(mainFoodCount == 1 ? 1 : mainFoodCount - 1)}>감소</button>
-    </div>
-  </>
-  )
-}
-
-const MemoizedOrderMainFood = React.memo(OrderMainFood);
-
-function OrderOptions({selectedCount, options, toggleAllchecked, btnAllChecked, toggleOptionCheck, optionCheckeds}) {
-  return (
-    <>
-    <h2 className='font-bold'>옵션 ({selectedCount} / {options.length})</h2>
-    <label>
-      <input type="checkbox" checked={btnAllChecked} onChange={toggleAllchecked}/>전체선택
-    </label>
-    <ul>
-      {options.map((option, index) => (
-        <li key={option}>
-          <label className='select-none cursor-pointer'>
-            <input type="checkbox" checked={optionCheckeds[index]} onChange={() => toggleOptionCheck(index)}/>
-            {option}
-          </label>
-        </li>
-      ))}
-    </ul>
-    </>
-  )
-}
-
-const MemoizedOrderOptions = React.memo(OrderOptions);
-
-function OrderDelivery({deliveryType, setDeliveryType}) {
-  return (
-    <>
-      <h2 className='font-bold'>배달 옵션</h2>
-      <label>
-        <input type="radio" name="delivery-type" checked={deliveryType == "직접수령"} onChange={() => setDeliveryType('직접수령')} />직접수령
-      </label>
-      <label>
-        <input type="radio" name="delivery-type" checked={deliveryType == "배달"} onChange={() => setDeliveryType('배달')} />배달
-      </label>
-    </>
-  )
-}
-
-const MemoizedOrderDelivery = React.memo(OrderDelivery);
-
-function Order() {
-  const [mainFoodCount, setMainFoodCount] = useState(1);
-  
-  const options = [
-    "머스타드", 
-    "케챱",
-    "마요네즈",
-    "갈릭",
-    "데리야끼",
-    "발사믹",
-    "칠리",
-    "치즈"
-  ];
-
-  const [optionCheckeds, setOptionCheckeds] = useState(new Array(options.length).fill(false));
-
-  const toggleOptionCheck = (index) => {
-    const newOptionCheckeds = optionCheckeds.map((el, _index) => _index == index ? !el : el);
-    setOptionCheckeds(newOptionCheckeds);
-  }
-
-  const btnAllChecked = useMemo(() => optionCheckeds.every((el) => el), [optionCheckeds]);
-  const selectedCount = useMemo(() => optionCheckeds.filter(el => el).length, [optionCheckeds]);
-
-  // useCallback으로 랜더링 줄이기
-  const toggleAllchecked = useCallback (() => {
-    if (btnAllChecked) {
-      const newOptionCheckeds = optionCheckeds.map((el) => false);
-      setOptionCheckeds(newOptionCheckeds);
-    }
-    
-    else {
-      const newOptionCheckeds = optionCheckeds.map((el) => true);
-      setOptionCheckeds(newOptionCheckeds);
-    }
-}, [optionCheckeds]);
-
-const [deliveryType, setDeliveryType ] = useState("직접수령");
-
-
-
-  return (
-    <>
-    <h1 className='text-xl font-bold'>음식주문</h1>
-
-    <MemoizedOrderMainFood setMainFoodCount={setMainFoodCount} mainFoodCount={mainFoodCount}/>
-
-    <MemoizedOrderOptions 
-      selectedCount={selectedCount}
-      options={options}
-      toggleAllchecked={toggleAllchecked}
-      btnAllChecked={btnAllChecked}
-      toggleOptionCheck={toggleOptionCheck}
-      optionCheckeds={optionCheckeds}/>
-
-    <MemoizedOrderDelivery deliveryType={deliveryType} setDeliveryType={setDeliveryType}/>
-
-    </>
-  );
-}
 
 export default function App() {
 
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (newTodo) => {
+    const newTodos = [...todos, todos.length + 1];
+    setTodos(newTodos);
+  }
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  }
+
+  const mdoifyTodo = (index, newTodo) => {
+    const newTodos = todos.map((todo, _index) => _index != index ? todo : newTodo);
+    setTodos(newTodos);
+  }
+
+  const onAddBtnClick = () => {
+    addTodo(todos.length + 1);
+  }
+
+  const onRemoveBinClick = () => {
+    removeTodo(1);
+  }
+
+  const onEditBtnClick = () => {
+    mdoifyTodo(1, "안녕");
+  }
+
   return (
-    <>
-    <Order/>
+
+    <> 
+      <div>{JSON.stringify(todos)}</div>
+      <button className="btn btn-info" onClick={onAddBtnClick}>추가</button>
+      <button className="btn btn-info" onClick={onRemoveBinClick}>삭제</button>
+      <button className="btn btn-info" onClick={onEditBtnClick}>수정</button>
     </>
   )
 }

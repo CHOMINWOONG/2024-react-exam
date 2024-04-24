e1797132f130032060bb6855d525d66e594d7efd
CHOMINWOONG <alsdnd0701@naver.com>
2024-04-24 21:55:35 +0900

    라우터 - 간단한 할 일 리스트

--- src/App.js
+++ src/App.js
@@ -1,111 +1,243 @@
 import React, { useState, useRef, useEffect, useMemo } from 'react';
 import {Routes, Route, Navigate, useLocation, NavLink, useParams, useNavigate} from "react-router-dom"
 import classNames from 'classnames';
+import { useRecoilState, atom } from 'recoil';
 
-function HomeMainPage() {
-  return (
-    <>
-      <h1>HOME, MAIN</h1>
-    </>
-  );
+const todosAtom = atom({
+  key: "app/todosAtom",
+  default: [
+    {id: 3, regDate: "2024-04-24 12:12:12", content: "운동"},
+    {id: 2, regDate: "2024-04-24 12:12:12", content: "공부"},
+    {id: 1, regDate: "2024-04-24 12:12:12", content: "코딩"}
+  ]
+});
+
+function useTodosState() {
+  const [todos, setTodos] = useRecoilState(todosAtom);
+  const lastTodoIdRef = useRef(todos.length == 0 ? 0 : todos[0].id);
+
+  const addTodo= (content) => {
+    // id값을 함수로 정의하여 입력할때마다 id값에 1씩 더하는 수가 변환될 수 있도록 current를 붙여줌
+    const id = ++lastTodoIdRef.current;
+    const regDate = "2024-04-24 12:12:12";
+
+    const newTodo = {
+      id,
+      regDate,
+      content
+    }
+
+    const newTodos = [newTodo, ...todos];
+    setTodos(newTodos);
+  }
+
+
+  const findIndexById = (id) => todos.findIndex((todo) => todo.id == id);
+
+  const findTodoById =
+
+  const removeTodoById = (id) => {
+    const index = findIndexById(id);
+
+    if ( index == -1 ) return;
+
+    const newTodos = todos.filter((_, _index) => index != _index);
+    setTodos(newTodos);
+  }
+
+  const modifyTodoById = (id, content) => {
+    const index = findIndexById(id);
+
+    if ( index == -1 ) return;
+
+    const newTodos = todos.map((todo, _index) => index == _index ? {...todo, content} : todo);
+    setTodos(newTodos);
+  }
+// 훅을 호출할때는 항상 중괄호 쓰기 {} !!!!!!!!!!!!!
+  return {
+    todos,
+    addTodo,
+    removeTodoById,
+    modifyTodoById
+  };
 }
 
-function ArticleDetailPage() {
-  const { id } = useParams();
-  const navigate = useNavigate();
+function TodoListItem({todo}) {
+  const todosState = useTodosState();
+  const [editMode, setEditMode] = useState(false);
 
+  const showEditMode = () => {
+    setEditMode(true);
+  }
+  
+  const cancelEditMode = () => {
+    setEditMode(false);
+  }
+
+  const commitEdit = () => {
+    setEditMode(false);
+  }
+
+  const onsubmitform = (e) => {
+    e.preventDefault();
+
+    const onSubmitEditForm = (e) => {
+      e.preventDefault();
+  
+      const form = e.target;
+  
+      form.content.value = form.content.value.trim();
+  // 아무것도 입력하지 않았을때 텍스트를 보이도록 한다.
+      if ( form.content.value.length == 0 ) {
+        alert('할 일을 입력해주세요.');
+        form.content.focus();
+  
+        return;
+      }
+
+      todosState.modifyTodoById(todo.id, form.content.value);
+      setEditMode(false);
+
+    commitEdit();
+  }
   return (
     <>
-      <h1>ARTICLE, DETAIL</h1>
-      <h1>{id}번 게시물 상세 페이지</h1>
-      <button onClick={() => navigate(-1)}>뒤로가기</button>
+      <li key={todo.id}>
+        {todo.id} : {todo.content}
+        <NavLink to={`/edit/$`}></NavLink>
+
+        <button className='btn btn-sm'
+        onClick={() => window.confirm(`${todo.id}번 할 일을 삭제하시겠습니까 ?`) && 
+        todosState.removeTodoById(todo.id)}>
+          삭제
+        </button>
+        
+      </li>
     </>
   )
 }
-// 리스트 항목을 만들때 id 값을 부여하여 ARTICLE 클릭시 게시물이 뜸
-function ArticleListPage() {
-  const articles = [
-    {
-      id: 1
-    },  
-    {
-      id: 2
-    },
-  ];
 
-  return (
+function TodoListPage() {
+  const todosState = useTodosState();
+
+  return(
     <>
-      <h1>ARTICLE, LIST</h1>
+      <h1>할 일 리스트</h1>
+
       <ul>
-        {articles.map((article) => (
-          <li key={article.id}>
-            <NavLink to={`/article/detail/${article.id}`}>
-              {article.id}번 게시물
-            </NavLink>
-          </li>
+        {todosState.todos.map((todo) => (
+          <TodoListItem key={todo.id} todo={todo}/>
         ))}
       </ul>
     </>
   );
 }
 
-function UserLoginPage() {
+function TodoEditPage() {
+  const Navigate = useNavigate();
+  const { id } = useParams();
+  const todosState = useTodosState();
+  const todo = todosState.findTodoById(id);
+
+  const form = e.target;
+  
+  form.content.value = form.content.value.trim();
+// 아무것도 입력하지 않았을때 텍스트를 보이도록 한다.
+  if ( form.content.value.length == 0 ) {
+    alert('할 일을 입력해주세요.');
+    form.content.focus();
+
+    return;
+  }
+
+  
   return (
     <>
-      <h1>USER, LOGIN</h1>
+    <h1>할 일 수정</h1>
+    <form onSubmit={onsubmitform} style={{display: "inline-block"}}>
+            <input 
+            className="input input-outlind input-sm" 
+            type="text" 
+            name="content" 
+            placeholder='할 일' 
+            defaultValue={todo.content} 
+            />
+            <button type='submit'>수정완료</button>
+            <button type='button' onClick={() => Navigate("/list")}>수정취소</button>
+          </form>
+    </>
+  )
+}
+
+function TodowritePage() {
+  const todosState = useTodosState();
+
+  
+  const form = e.target;
+  
+  form.content.value = form.content.value.trim();
+// 아무것도 입력하지 않았을때 텍스트를 보이도록 한다.
+  if ( form.content.value.length == 0 ) {
+    alert('할 일을 입력해주세요.');
+    form.content.focus();
+
+    return;
+  }
+
+    todosState.addTodo(form.content.value);
+    // 입력 후에 입력창을 초기값으로 돌려놓는다.
+    form.content.value = "";
+    form.content.focus();
+  
+    
+  }
+  return(
+    <>
+      <h1>할 일 작성</h1>
+      {/* Submit을 만들때는 항상 form태그를 쓰고 안에는 input이나 button을 쓴다.  */}
+      <form onSubmit={onSubmit}>
+        <input className="input input-bordered my-5"autoComplete="off" name="content" type="text" placeholder="할 일을 입력해주세요." />
+        <input type="submit" value="작성" className='input input-bordered ml-2' />
+      </form>
+      <div>
+        {todosState.todos.length}
+      </div>
     </>
   );
 }
 
+
 export default function App() {
   const location = useLocation();
-  
+
   return (
     <> 
-    <span>현재 주소 : {location.pathname}</span>
-
-    <hr />
-
-{/* NavLink 링크를 달고 싶을때 씀 */}
-    <div>
+    <header>
+      {/* style : 현재 위치하고 있는 창일때 red색으로 변환하는 스타일 추가 */}
       <NavLink 
-      to="/home/main" 
-      // isActive 조건문을 달아서 클릭했을때 안했을때 상태 변환설정
-      className={({ isActive }) => classNames(
-        "btn", 
-        {"btn-link" : !isActive },
-        {"btn-primary" : isActive }
-      )}>
-        HOME, MAIN 페이지로 이동
+        to="/list"
+        style={({ isActive }) => ({ color: isActive ? "red" : null })}
+      >
+        리스트
       </NavLink>
-
+      &nbsp;/&nbsp;
+      {/* style : 현재 위치하고 있는 창일때 red색으로 변환하는 스타일 추가 */}
       <NavLink 
-      to="/article/list" 
-      className={({ isActive }) => classNames(
-        "btn", 
-        {"btn-link" : !isActive },
-        {"btn-primary" : isActive }
-      )}>
-        ARTICLE, LIST 페이지로 이동
+        to="/write"
+        style={({ isActive }) => ({ color: isActive ? "red" : null })}
+        >
+        작성
       </NavLink>
+      <hr />
+      주소 : {location.pathname}
+    </header>
 
-      <NavLink 
-      to="/user/login" 
-      className={({ isActive }) => classNames(
-        "btn", 
-        {"btn-link" : !isActive },
-        {"btn-primary" : isActive }
-      )}>
-        USER, LOGIN 페이지로 이동
-      </NavLink>
-    </div>
-      <Routes>
-        <Route path="/home/main" element={<HomeMainPage />} />
-        <Route path="/ARTICLE/LIST" element={<ArticleListPage />} />
-        <Route path="/ARTICLE/DETAIL" element={<ArticleDetailPage />} />
-        <Route path="/user/login" element={<UserLoginPage />} />
-        <Route path="*" element={<Navigate to="/user/login" />} />
-      </Routes>
+    <Routes>
+      <Route path="/list" element={<TodoListPage />} />
+      <Route path="/write" element={<TodowritePage />} />
+      {/* write 페이지로 돌아가는  */}
+      <Route path="*" element={<Navigate to="/write" />} />
+    </Routes>
     </>
   );
 }
